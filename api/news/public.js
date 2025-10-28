b61da72ce8682de3982cdfec49f6289a7e2bf57d
Julien <ju.richard.33@gmail.com>
2025-10-27 21:15:18 -0400

    new updates

--- api/news/public.js
+++ api/news/public.js
@@ -1,16 +1,6 @@
 export default async function handler(req, res) {
-    // CORS headers
-    const allowedOrigins = [
-        'https://prod-julien.vercel.app',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000'
-    ];
-
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
+    // CORS headers - Allow all origins for now
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
